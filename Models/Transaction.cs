eed3c43e2aaa7cf37a7a832526d45678d2b92b27
dyank432 <dyank432@mtroyal.ca>
2024-04-05 16:52:55 -0600

    created basic dashboard + sidebar layout

--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -9,10 +9,11 @@ namespace Expense_Tracker.Models
         [Key]
         public int TransactionId { get; set; }
 
-        //CategoryId
+        [Range(1,int.MaxValue, ErrorMessage = "Please select a category.")]
         public int CategoryId { get; set; }
         public Category? Category { get; set; }
 
+        [Range(1, int.MaxValue, ErrorMessage = "Amount should be greater than 0.")]
         public int Amount { get; set; }
 
         [Column(TypeName = "nvarchar(75)")]
@@ -20,5 +21,22 @@ namespace Expense_Tracker.Models
 
         public DateTime Date { get; set; } = DateTime.Now;
 
+        [NotMapped]
+        public string? CategoryTitleWithIcon
+        {
+            get
+            {
+                return Category == null ? "" : Category.Icon + " " + Category.Title;
+            }
+        }
+
+        [NotMapped]
+        public string? FormattedAmount
+        {
+            get
+            {
+                return ((Category == null || Category.Type == "Expense") ? "- " : "+ ") + Amount.ToString("C0");
+            }
+        }
     }
 }
